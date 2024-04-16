6c978965ef1e3b727931dcb55c88989b48f1cfce
katushenca <shutenko.katya@bk.ru>
2024-04-16 13:42:19 +0500

    create run and jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]private float speed; //[SerializeField] чтобы появился ползунок в unity
     private Rigidbody2D body; // чтобы управлять слаймом
-
+    private Animator animator; // доступ к аниматору
+    private bool grounded;
+    
     private void Awake() // Вызывается каждый раз когда начинается игра
     {
         body = GetComponent<Rigidbody2D>(); // Получить доступ к слайму - проверить player на наличие rigidbody2d и будет хранить внутри body
         // условно говоря получение ссылки
+        animator = GetComponent<Animator>();
     } 
 
     private void Update()
@@ -23,7 +27,25 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = Vector3.one; // чтоб лицом поворачивался в нужную сторону
         if (horizontalInput < -0.01f) // двигается вправо
             transform.localScale = new Vector3(-1, 1, 1); // чтоб лицом поворачивался в нужную сторону 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded) // прыгать можно только когда нажат пробел и челик на земле
+            Jump();
+        
+        // Set Animations
+        animator.SetBool("run", horizontalInput != 0);
+        animator.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        // с тригером анимация правильная
+        animator.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) // Встроенный метод
+    {
+        if (collision.gameObject.tag == "ground")
+            grounded = true;
     }
 }
